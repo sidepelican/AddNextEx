e81091166e73c48a1b5f296d5cc54ff694ea7353
kenta_okamura <side.junktown@gmail.com>
2018-11-01 23:30:52 +0900

    init project

---
+++ AddNextEx/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  AddNextEx
+//
+//  Created by kenta on 2018/11/01.
+//  Copyright © 2018 iceman. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
