b8e5d68320be9911f306709e1e5a03ba09d2e37d
kenta_okamura <side.junktown@gmail.com>
2018-11-04 00:33:59 +0900

    find with top term

--- AddNextEx/SourceEditorCommand.swift
+++ AddNextEx/SourceEditorCommand.swift
@@ -20,27 +20,72 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         }
 
         let selected = (buffer.lines[firstSelection.start.line] as! NSString).substring(with: NSRange(location: firstSelection.start.column, length: firstSelection.end.column - firstSelection.start.column))
+        if selected.isEmpty {
+            completionHandler(nil)
+            return
+        }
 
-        var index = lastSelection.end.line
-        var column = lastSelection.end.column
-        let linesCount = buffer.lines.count
-        while index < linesCount {
+        let lastIndex = buffer.lines.count - 1
+
+        // search in bottom term
+        if let found = find(buffer: buffer,
+                            range: .init(start: lastSelection.end,
+                                         end: .init(line: lastIndex, column: buffer.lineLength(at: lastIndex) - 1)),
+                            searchText: selected) {
+            buffer.selections.insert(found, at: buffer.selections.count - 1)
+            completionHandler(nil)
+            return
+        }
+
+        // search in top term
+        if let found = find(buffer: buffer,
+                            range: .init(start: .init(line: 0, column: 0),
+                                         end: lastSelection.start),
+                            searchText: selected) {
+            buffer.selections.insert(found, at: buffer.selections.count - 1)
+            completionHandler(nil)
+            return
+        }
+
+        completionHandler(nil)
+    }
+
+    private func find(buffer: XCSourceTextBuffer, range: XCSourceTextRange, searchText: String) -> XCSourceTextRange? {
+        var index = range.start.line
+        var column = range.start.column
+        while index <= range.end.line {
             let line = buffer.lines.object(at: index) as! NSString
-            let found = line.range(of: selected, options: [.caseInsensitive], range: NSRange(location: column, length: line.length - column))
+            let lineRange: NSRange
+            if index == range.end.line {
+                lineRange = NSRange(location: column, length: range.end.column)
+            } else {
+                lineRange = NSRange(location: column, length: line.length - column)
+            }
+
+            let found = line.range(of: searchText,
+                                   options: [.caseInsensitive],
+                                   range: lineRange)
             if found.location != NSNotFound {
                 let next = XCSourceTextRange()
                 next.start = XCSourceTextPosition(line: index, column: found.lowerBound)
                 next.end = XCSourceTextPosition(line: index, column: found.upperBound)
-                buffer.selections.insert(next, at: buffer.selections.count - 1)
 
-                completionHandler(nil)
-                return
+                if !buffer.selections.contains(next) {
+                    return next
+                }
             }
 
             column = 0
             index += 1
         }
 
-        completionHandler(nil)
+        return nil
+    }
+}
+
+private extension XCSourceTextBuffer {
+    func lineLength(at index: Int) -> Int {
+        let line = lines.object(at: index) as! NSString
+        return line.length
     }
 }
